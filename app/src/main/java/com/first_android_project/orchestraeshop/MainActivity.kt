cc4f5580657c430447fe00b3e1c5494bcf5f2a7d
Asm Nahin <asmnahin21@gmail.com>
2022-02-21 00:46:26 +0600

    Main to Register

--- app/src/main/java/com/first_android_project/orchestraeshop/MainActivity.kt
+++ app/src/main/java/com/first_android_project/orchestraeshop/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.first_android_project.orchestraeshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.first_android_project.orchestraeshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainRegisterButton.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
     }
 }
